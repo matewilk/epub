3c474c9eebbac66f16c9c533d334a84179a4efdd
Mateusz Wilk <matewilk@gmail.com>
2015-04-05 15:37:19 +0100

    Gruntfile: watch

--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -18,11 +18,22 @@ module.exports = function(grunt){
                 reporter: require('jshint-stylish')
             },
             all: ['Gruntfile.js', 'app/scripts/**/*.js', 'test/spec/**/*.js']
+        },
+
+        watch: {
+            handlebars: {
+                files: 'app/scripts/templates/*.hbs',
+                tasks: ['handlebars'],
+                options: {
+                    livereload: true
+                }
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-handlebars');
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
     grunt.registerTask('default', ['handlebars', 'jshint']);
 };
