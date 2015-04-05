053d61b3e8813283586ab92ac9221264cf1158b9
Mateusz Wilk <matewilk@gmail.com>
2015-04-05 14:52:38 +0100

    Gruntfile: handlebars task

--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -1,6 +1,19 @@
 module.exports = function(grunt){
 
     grunt.initConfig({
-
+        handlebars: {
+            compile: {
+                options: {
+                    amd: true
+                },
+                files: {
+                    '.tmp/scripts/templates.js': 'app/scripts/templates/*.hbs'
+                }
+            }
+        }
     });
+
+    grunt.loadNpmTasks('grunt-contrib-handlebars');
+
+    grunt.registerTask('default', ['handlebars']);
 };
