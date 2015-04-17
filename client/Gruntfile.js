a59cca2382bc8a61923e1e1be3f782d6ffa3220d
Mateusz Wilk <mateusz.wilk@bbc.co.uk>
2015-04-17 14:02:25 +0100

    change grunt-contrib-sass to grunt-sass

--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -76,7 +76,7 @@ module.exports = function(grunt){
 
     grunt.loadNpmTasks('grunt-contrib-handlebars');
     grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-copy');
