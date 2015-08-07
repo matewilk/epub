6ab1b479564d769fff4f66d5f9f9307164fc8135
Mateusz Wilk <mateusz.wilk@bbc.co.uk>
2015-08-07 19:36:39 +0100

    Added react/backbone-component, translation dialog proof of concept

--- client/app/scripts/epubhooks/selection.js
+++ client/app/scripts/epubhooks/selection.js
@@ -7,6 +7,22 @@ define(function(require){
         $(renderer.element).contents().find('body').bind('mouseup', function(){
 
             var selection = $(renderer.element).contents()[0].getSelection().toString();
+            var range = $(renderer.element).contents()[0].getSelection().getRangeAt(0).getBoundingClientRect();
+
+
+            var relative = document.body.parentNode.getBoundingClientRect();
+            var element = document.createElement('div');
+
+            document.body.appendChild(element);
+
+            element.style.width = '50px';
+            element.style.height = '50px';
+            element.style.backgroundColor = 'red';
+            element.style.position = 'absolute';
+
+            element.style.top =(range.bottom - relative.top)+'px';
+            element.style.right=-(range.right - relative.right + ((10 / 100)*relative.right))+'px';
+
             console.log(selection);
         });
 
