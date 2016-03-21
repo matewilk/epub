0dc6d29d9e85c11ddc28a9c4a7df156b7638f6e8
Mateusz Wilk <mateusz.wilk@bbc.co.uk>
2016-03-21 17:24:36 +0000

    Proper app launch

--- server/codedeploy-scripts/install-packages.sh
+++ server/codedeploy-scripts/install-packages.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-cd /home/ubuntu/epub/server/
-npm install
-
 #install nodemon on (part of the package.json)
 #install mongodb
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
@@ -13,13 +10,21 @@ sudo service mongod start
 
 ln -s /usr/bin/nodejs /usr/bin/node
 
+sudo apt-get install -y git
+npm install -g grunt-cli bower
 
 cd /home/ubuntu/epub/client
 npm install
 bower install --allow-root
+grunt
 
-#grunt handlebars and similar
+cd /home/ubuntu/epub/server/
+npm install
+
 export MONGOLAB_URI="mongodb://localhost:27017/test"
-npm start
+
+#npm start
+#use npm run dev for now
+npm run dev
 
 
